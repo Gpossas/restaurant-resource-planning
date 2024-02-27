5c097899147ea426ef79962321b8292814fd5035
Gpossas <possasg@gmail.com>
2024-02-27 08:40:48 -0300

    fix: schema input type and change address name

--- app/server/models/restaurant.py
+++ app/server/models/restaurant.py
@@ -1,7 +1,7 @@
 from datetime import time
 from pydantic import BaseModel, Field
 from server.models.image import Image
-from server.models.adress import Adress
+from server.models.address import Address
 from server.models.dish import Dish, PyObjectId
 
 
@@ -12,7 +12,7 @@ class RestaurantSchema( BaseModel ):
     name: str = Field( ... )    
     slug: str = Field( ... )
     category: str = Field( ... )
-    adress: Adress = Field( ... )
+    address: Address = Field( ... )
     # dishes: list[Dish] = Field( ... )
     description: str | None = None
     logo: Image | None = None
@@ -22,37 +22,44 @@ class RestaurantSchema( BaseModel ):
     
     model_config = {
         "json_schema_extra": {
-            "examples": {
-                "cnpj": "12345678000100",
-                "name": "Morama",
-                "slug": "morama",
-                "category": "Candy store",
-                "adress": {
-                    "street": "Somewhere over the rainbow, 12",         
-                    "complement": "High above the chimney tops"
-                },
-                "dishes": [
-                    {
-                        "name": "chocolate cake",
-                        "price": 316.23
+            "examples": [
+                {
+                    "cnpj": "12345678000100",
+                    "name": "Morama",
+                    "slug": "morama",
+                    "category": "Candy store",
+                    "address": {
+                        "cep": "60130240",
+                        "street": "Rua Dr. Oswaldo Cruz",
+                        "residence_number": 476,
+                        "neighborhood": "Bebedouro",
+                        "city": "Maceio",
+                        "state": "AL",
+                        "complement": "De 2 Até 1550 Lado Par",
                     },
-                    {
-                        "name": "vanilla ice cream",
-                        "price": 9.99
+                    "dishes": [
+                        {
+                            "name": "chocolate cake",
+                            "price": 316.23
+                        },
+                        {
+                            "name": "vanilla ice cream",
+                            "price": 9.99
+                        },
+                    ],
+                    "description": "Morama is a candy shop that was created by 3 friends in 2024",
+                    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wfla.com%2Fbloom-tampa-bay%2F10-surprising-benefits-of-having-a-cat-in-your-life%2F&psig=AOvVaw0TUkLGDFWdSmBOfDe38731&ust=1708977051859000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOiZuuKhx4QDFQAAAAAdAAAAABAE",
+                    "opening_hours": {
+                        "monday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
+                        "tuesday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
+                        "wednesday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
+                        "thursday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
+                        "friday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
+                        "saturday": [ ( time(7, 0), time(12, 0) ) ],
+                        "sunday": [],
                     },
-                ],
-                "description": "Morama is a candy shop that was created by 3 friends in 2024",
-                "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wfla.com%2Fbloom-tampa-bay%2F10-surprising-benefits-of-having-a-cat-in-your-life%2F&psig=AOvVaw0TUkLGDFWdSmBOfDe38731&ust=1708977051859000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOiZuuKhx4QDFQAAAAAdAAAAABAE",
-                "opening_hours": {
-                    "monday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
-                    "tuesday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
-                    "wednesday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
-                    "thursday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
-                    "friday": [ ( time(7, 0), time(11, 45) ), ( time(14, 0), time(6, 0)) ],
-                    "saturday": [ ( time(7, 0), time(12, 0) ) ],
-                    "sunday": [],
-                },
-                "payment_methods": ["Pix", "Credit card"]
-            }
+                    "payment_methods": ["Pix", "Credit card"]
+                }
+            ]       
         }
     }
